0ee47584b62d5caac335b15b5971ede26367f75e
Md. Abu Taher <entrptaher@gmail.com>
2018-11-02 22:01:43 +0600

    Start of solution

--- task-1/solutions/cleaner.js
+++ task-1/solutions/cleaner.js
@@ -1,5 +1,10 @@
 module.exports = function cleaner(input) {
+  input = JSON.parse(JSON.stringify(input))
+  // make shallow copy of input to remove reference
+  const output = {};
+  // clean up "css_args" as it takes a lot of useless space
   input.params.data.selectorArray.forEach((e, i, a) => {
+    // modifies original array
     a[i].css_args = '';
   });
   return input;
--- task-1/solutions/solution.js
+++ task-1/solutions/solution.js
@@ -3,18 +3,25 @@ const path = require('path');
 
 const inputDir = path.resolve(`${__dirname}/../input`);
 const outputDir = path.resolve(`${__dirname}/../output`);
+
+// reads the input file line by line
 const lineReader = require('readline').createInterface({
   input: fs.createReadStream(`${inputDir}/raw.txt`),
 });
 const cleaner = require('./cleaner');
 
 const baseJSON = [];
+// asynchronously processes all lines
 lineReader.on('line', (line) => {
   console.log('Current Input', baseJSON.length);
+  // cleanup and format data properly
+  // Every line is a JSON object exported from mongoDB
   const newData = cleaner(JSON.parse(line));
+  // save the data
   baseJSON.push(newData);
 });
 lineReader.on('close', () => {
   console.log('Total Processed', baseJSON.length);
-  fs.writeFileSync(`${inputDir}/output.json`, JSON.stringify(baseJSON, true, 2));
+  // write on disk only after all lines are processed
+  fs.writeFileSync(`${outputDir}/output.json`, JSON.stringify(baseJSON, true, 2));
 });
